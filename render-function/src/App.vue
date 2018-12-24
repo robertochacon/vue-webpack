4b3a144e55c7699bc60b4396facbff4b8699716b
Roberto Chacon A <tito.123chacon@gmail.com>
2018-12-24 00:06:37 -0400

    funcion render prueva

---
+++ render-function/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <elemento tipo="header">este es el header</elemento>
+</template>
+
+<script>
+
+import elemento from './components/elemento.vue';
+export default {
+  components: {elemento}
+};
+</script>
+
+<style></style>
